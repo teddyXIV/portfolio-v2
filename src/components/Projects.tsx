a35c21caa58c9fd75e91fef0dd4d0bb51b9281ec
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-01-05 15:24:06 -0800

    add all projects to project component.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,8 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 const Projects = () => {
     return (
@@ -16,10 +18,54 @@ const Projects = () => {
                     <Typography variant="body2">
                         A web app exploring the implementation of APIs with React
                     </Typography>
+                    <CardActions>
+                        <Button size="small" href="https://github.com/teddyXIV/react-api-recipes" target='_blank'>{<GitHubIcon />}</Button>
+                    </CardActions>
+                    <Stack direction="row" spacing={1}>
+                        <Chip label="React" />
+                        <Chip label="Vite" />
+                        <Chip label="MUI" />
+                        <Chip label="API calls" />
+                    </Stack>
                 </CardContent>
-                <CardActions>
-                    <Button size="small" href="https://github.com/teddyXIV/react-api-recipes" target='_blank'>{<GitHubIcon />}</Button>
-                </CardActions>
+            </Card>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        Inventory Tracker
+                    </Typography>
+                    <Typography variant="body2">
+                        A inventory tracking app for a fictional surf shop.
+                    </Typography>
+                    <CardActions>
+                        <Button size="small" href="https://github.com/teddyXIV/redux-inventory-tracker.git" target='_blank'>{<GitHubIcon />}</Button>
+                    </CardActions>
+                    <Stack direction="row" spacing={1}>
+                        <Chip label="React" />
+                        <Chip label="Redux Toolkit" />
+                        <Chip label="TypeScript" />
+                        <Chip label="Vite" />
+                    </Stack>
+                </CardContent>
+            </Card>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        Currency exchanger
+                    </Typography>
+                    <Typography variant="body2">
+                        An application that allows the user to enter a currency and convert it into another currency at the current exchange rate.
+                    </Typography>
+                    <CardActions>
+                        <Button size="small" href="https://github.com/teddyXIV/Currency-Exchanger.git" target='_blank'>{<GitHubIcon />}</Button>
+                    </CardActions>
+                    <Stack direction="row" spacing={1}>
+                        <Chip label="JavaScript" />
+                        <Chip label="API calls" />
+                        <Chip label="Vite" />
+                        <Chip label="Bootstrap" />
+                    </Stack>
+                </CardContent>
             </Card>
         </>
     )
