162f27277083edc36feec6ee4c31d0c9c6929b75
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-01-05 15:39:49 -0800

    add inital AboutMe content

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,9 @@
+const AboutMe = () => {
+    return (
+        <p>As a former fisheries biologist on the Bering Sea, I've transitioned to the world of web development, swapping the rocking boat for a stable standing desk. From battling waves to debugging code, I've learned that adaptability is key, whether at sea or in the ever-evolving landscape of programming—now tackling bugs instead of halibut.
+
+            I'm currently immersed in coding school with Epicodus, honing skills and unlocking new ones. Currently exploring Javascript, React, and C#. Really looking forward to implementing what I've learned at Epicodus while learning new skills on the job.</p>
+    )
+}
+
+export default AboutMe;
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import Header from './Header'
 import './App.css'
 import Experience from './Experience'
 import Projects from './Projects'
+import AboutMe from './AboutMe'
 
 function App() {
 
   return (
     <>
       <Header />
+      <AboutMe />
       <Experience />
       <Projects />
     </>
